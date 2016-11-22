47dfc7d01a7a985ad85da5c8f5a5e6af6fc5a0f4
lick here to get my dull name <wingsuit@hot.ee>
2016-11-22 19:29:50 +0000

    remove auth data

--- db.php
+++ db.php
@@ -1,21 +1,16 @@
 <?php
     
-    if (PRODUCTION) {
-        $dbUser = 'd60840_collectio';
-        $dbPassword = 'sd!v908%w2k2m2f8f#';
-        $dbHost='d60840.mysql.zonevs.eu';
-        $userDb='d60840_ilmainfo';
-    } elseif (DEV) {
-        $dbUser = 'root';
-        $dbPassword = '';
-        $dbHost='127.0.0.1';
-        $userDb='d60840_ilmainfo';
-    }
+require 'auth.php';
+
+    $mysqli = new mysqli($dbHost, $dbUser, $dbPassword, $userDb);
     
-    
-    if (!$mysqli = new mysqli($dbHost, $dbUser, $dbPassword, $userDb)){
+    if ($mysqli->connect_errno > 0){
         logError("Cannot connect to mysql database: \n".$mysqli->connect_error);
     }
+
+    
+    //var_dump($mysqli);
+    
     $mysqli->set_charset("utf8");
     
 ?>
